450fcc7822cf2b512c25a68d887f7549117f50b5
GhosTHaise <Ghostrex2@gmail.com>
2023-02-19 22:02:43 +0300

    Create root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,10 @@ function App() {
           </header>
 
           <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-[73px])]'>
-
+              <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/create-post' element={<CreatePost />} />
+              </Routes>
           </main>
       </BrowserRouter>
   )
