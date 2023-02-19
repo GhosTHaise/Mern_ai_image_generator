160556ea329860cff0de41e1ac88a2c138aab7f0
GhosTHaise <Ghostrex2@gmail.com>
2023-02-19 12:21:27 +0300

    Create and init header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { BrowserRouter,Link,Routes,Route } from 'react-router-dom'
-import './App.css'
-
+import { logo } from './assets'
+import { Home,CreatePost } from './Pages'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
