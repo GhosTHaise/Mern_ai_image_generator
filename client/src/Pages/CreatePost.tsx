92354bfd927f4e23e147debdb15b8666216ec8c0
GhosTHaise <Ghostrex2@gmail.com>
2023-02-21 18:25:07 +0300

    create button

--- client/src/Pages/CreatePost.tsx
+++ client/src/Pages/CreatePost.tsx
@@ -16,8 +16,12 @@ const CreatePost = (props: Props) => {
       photo : ""
   });
 
-  const [generating,setGeneratingImg] = useState(false);
+  const [generatingImg,setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false)
+
+  const generateImage = () =>{
+
+  }
 
   const handleSubmit = () => {
 
@@ -47,7 +51,7 @@ const CreatePost = (props: Props) => {
         className='mt-16 max-w-3xl'
         onSubmit={handleSubmit}
         >
-            <div className='felx flex-col gap-5'>
+            <div className='flex flex-col gap-5'>
                 <FormField
                 labelName="Your Name"
                 type="text"
@@ -66,6 +70,55 @@ const CreatePost = (props: Props) => {
                 isSurpriseMe={true}
                 handleSurpriseMe={handleSurpriseMe}
                 />
+                <div className='relative bg-gray-50 border botder-gray-300
+                 text-gray-900 text-sm
+                  rounded-lg focus:ring-blue-500 focus:border-blue-500 
+                  w-64 p-3 h-64 flex justify-center items-center
+                '>
+                    {form.photo ? (
+                      <img 
+                      src={form.photo} 
+                      alt={form.prompt} 
+                      className="w-full h-full object-contain" />
+                    ) : 
+                    (
+                      <img 
+                      src={preview} 
+                      alt="preview" 
+                      className='w-9/12 h-9/12 object-contain opacity-40' />
+                    )}
+                    {generatingImg && (
+                      <div className='absolute inset-0 z-0 flex justify-center
+                          items-center bg-[rgba(0,0,0,0.5)] rouned-lg'>
+                        <Loader />
+                      </div>
+                    )}
+                </div>
+            </div>
+            <div className='mt-5 flex gap-5'>
+                      <button 
+                      type='button' 
+                      onClick={generateImage}
+                      className="text-white bg-green-700 font-medium rounded-md
+                      text-sm w-full sm:w-auto px-5 py-2.5 text-center
+                      "
+                      >
+                          {generatingImg ? "Generating..." : "Generate"}
+                      </button>
+            </div>
+            <div className='mt-10'>
+                  <p className='mt-2 text-[#666e75] text-[14px]'>
+                      Once you have created the image you want, you 
+                      can share it with others in the community.
+                  </p>
+                  <button
+                    type='submit'
+                    className='mt-3 text-white bg-[#6469ff] font-medium
+                      rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center
+                    '
+                  > 
+                        {loading ? "Sharing..." : "Share with the community"}
+                  </button>
             </div>
         </form>
     </section>
