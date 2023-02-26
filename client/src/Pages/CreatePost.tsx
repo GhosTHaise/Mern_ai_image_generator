e3bb314bb463bfbcb57a7cac068b46c2b3c08639
GhosTHaise <Ghostrex2@gmail.com>
2023-02-26 19:27:28 +0300

    Create post route

--- client/src/Pages/CreatePost.tsx
+++ client/src/Pages/CreatePost.tsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{ReactEventHandler, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 import { preview } from '../assets'
 import { getRandomPrompt } from '../utils'
@@ -40,11 +40,28 @@ const CreatePost = (props: Props) => {
     }else{
       alert("Please enter a prompt.")
     }
-    console.log(form.photo)
+    //console.log(form.photo)
   }
 
-  const handleSubmit = () => {
-
+  const handleSubmit = async (e : React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(form.prompt && form.photo){
+      setLoading(true);
+      try {
+          const response = await fetch("http://localhost:8080/api/v1/post",
+          {
+            method : "POST",
+            headers : {
+              "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(form)
+          }
+          )
+          await response.json();
+      } catch (error) {
+        
+      }
+    }
   }
 
 
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -3,10 +3,47 @@ import * as dotenv from "dotenv";
 
 import {v2 as cloudinary} from "cloudinary";
 
-import PosT from "../mongodb/models/post.js";
+import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const Router = express.Router();
+
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+});
+
+/* Get all post */
+Router.route("/").get(async(req,res)=>{
+    try {
+        const posts = await Post.find({});
+
+        res.status(200).json({success : true,data : posts});
+    } catch (error) {
+        res.status(500).json({success : false,message : error});
+    }
+});
+
+/* create a post */
+Router.route("/").post(async(req,res)=>{
+    try {
+        const {name,prompt,photo} = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    /* Create post on our db */
+    const newPost = await Post.create({
+        name,
+        prompt,
+        photo : photoUrl.url
+    });
+
+    res.status(201).join({success : true,data : newPost});
+    } catch (error) {
+        res.status(500).json({success : false,message : error});
+    }
+});
 
 export default Router;
